cbcc00853cce5d00f33d0c58034df5eeada7cd71
Maksym Chepurko <maksym.chepurko@danads.se>
2022-11-13 22:09:24 +0100

    Changes: 1. Added conditions: messages ==5, or time delay = 10 sec.

--- src/main/java/com/example/recievingmessage/ReceivingMessageApplication.java
+++ src/main/java/com/example/recievingmessage/ReceivingMessageApplication.java
@@ -1,29 +1,50 @@
 package com.example.recievingmessage;
 
 import java.nio.charset.StandardCharsets;
+import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ReceivingMessageApplication {
 
+  private static Logger log = LoggerFactory.getLogger(ReceivingMessageApplication.class);
+
   private final static String QUEUE_NAME = "hello";
+  private final static long START_TIME = System.currentTimeMillis();
+  private final static long WAITE_TIME = 10000L;
+
+  private static DeliverCallback getDeliveryCallBack() {
+
+    return (consumerTag, delivery) -> {
+      String msg = new String(delivery.getBody(), StandardCharsets.UTF_8);
+      System.out.println("Message: " + msg);
+    };
+  }
 
   public void getMessages() throws Exception {
+
     ConnectionFactory factory = new ConnectionFactory();
     factory.setHost("localhost");
     Connection connection = factory.newConnection();
     Channel channel = connection.createChannel();
 
-    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+    AMQP.Queue.DeclareOk dok = channel.queueDeclare(QUEUE_NAME, false, false, false, null);
     System.out.println("Waiting for messages. To exit press CTRL+C");
 
-    DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-      String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
-      System.out.println("Received: '" + message + "'");
-    };
-    channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
-  }
+    if (dok.getMessageCount() == 5) {
+       channel.basicConsume(QUEUE_NAME, true, getDeliveryCallBack(), consumerTag -> {
+      });
+    }
 
+    while ((START_TIME + WAITE_TIME) > System.currentTimeMillis()){
+      Thread.sleep(100);
+    }
+
+    channel.basicConsume(QUEUE_NAME, true, getDeliveryCallBack(), consumerTag -> {
+    });
+  }
 }
